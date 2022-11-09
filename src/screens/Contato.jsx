9897b4c4c0a3dc768502d64c04764846a8d99685
John Doe <johndoe@example.com>
2022-11-09 15:28:19 -0300

    commit final

--- src/screens/Contato.jsx
+++ src/screens/Contato.jsx
@@ -3,32 +3,8 @@ import { Button, Text, TextInput, View } from "react-native";
 import { styles } from "./lib/styles";
 
 export const Contato = ({  RootNavigation }) => {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-
-    return(
-        <View style={styles.container}>
-            <TextInput 
-            label="Email"
-            mode="outlined"
-            value={email}
-            onChangeText={(emailDigitado)=> setEmail(emailDigitado)}
-        />
-        <TextInput
-        label="Senha"
-        mode="outlined"
-        value={password}
-        onChangeText={(valor) => setPassword(valor)}
-        secureTextEntry={true}
-        />
-        <Button
-        style={styles.button}
-        mode="outlined"
-        label="Entrar"
-        onPress={()=> navigation.navigate("Home")}
-        >
-        </Button>
-        </View>
-    );
+<View>
+    <Text>Central de atendimento é pelo email, número de telefone</Text>
+</View>
 };
   
